f1da6ec6b940511712177a9f5af50cf0e9f182d3
Joshua Cilufo <joshua.cilufo@student.swic.edu>
2025-09-03 20:38:30 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+// Use const for values that don't change
+const greeting = "Hello, World!";
+
+// Named constant instead of magic number
+const LEGAL_DRINKING_AGE = 21;
+
+// Function with fewer parameters
+function checkAge(age) {
+  return age >= LEGAL_DRINKING_AGE ? "adult" : "minor";
+}
+
+// Use array literal notation
+const grades = [85, 92, 78];
+
+// Use console.info for general output
+console.info("Application started");
